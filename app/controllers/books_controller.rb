33fb36d715ebc1386c1742d8208e45ed39003dcd
AyaneNishikawa <149765130+ayane0814@users.noreply.github.com>
2023-12-05 15:42:06 +0000

    [Add] 新規投稿の作成中途半端

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,32 @@
 class BooksController < ApplicationController
+    def create
+        book = Book.new(book_params)
+        book.save
+        # ユーザーの詳細に飛べない、コントローラーの設定してないからとか？
+        redirect_to user_path
+    end
+    
+    def index
+        @book = Book.new
+        
+        @books = Book.all
+    end
+    
+    def show
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+    
+    private
+    
+    def book_params
+        params.require(:book).permit(:title, :body)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    def index
+    end
+    
+    def show
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    private
+    
+    def user_params
+        params.require.(:user).permit(:name, :introduction,)
+    end
 end
