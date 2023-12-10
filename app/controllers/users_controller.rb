1f4cd16a382932a303b92a12f728724f762f746b
AyaneNishikawa <149765130+ayane0814@users.noreply.github.com>
2023-12-10 10:37:48 +0000

    [Add] bootstrap作成完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,11 @@ class UsersController < ApplicationController
     end
     
     def update
-        user = User.find(params[:id])
-        user.update(user_params)
-        redirect_to user_path(user.id)
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "You have updated user successfully."
+            redirect_to user_path(user.id)
+        end
     end
     
     private
